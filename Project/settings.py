abc5740ec012c367c7c0f08291cb60da32cfb4b9
kaelzubs <donmart4u@gmail.com>
2019-09-30 16:13:52 +0100

    initial commit

--- Project/settings.py
+++ Project/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',
             ],
             'loaders': [
                 ('django.template.loaders.cached.Loader', [
@@ -143,8 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')],
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 EMAIL_HOST = 'smtp.sendgrid.net'
